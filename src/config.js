ab5ad64c99bbd341bb91db274032aebcc29efbf8
iamxadnan <adnanshariffpc@gmail.com>
2025-07-23 00:28:08 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config={
-    "url":"https://asksdperpproject-production.up.railway.app"//backend url
+    // "url":"https://asksdperpproject-production.up.railway.app"//backend url
     // "url":"http://localhost:2001"
+     "url":"https://ask-sdp-erp-project.onrender.com/"
 }
 export default config
